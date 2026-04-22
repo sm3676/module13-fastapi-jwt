21dba1008c9b7ae970f44558300a6b8d18055942
Sharvani Rao <sm3676@njit.edu>
2026-04-21 21:51:53 -0400

    fix tests properly

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,18 +1,33 @@
 from fastapi.testclient import TestClient
 from app.main import app
+import uuid
 
 client = TestClient(app)
 
 def test_register():
+    email = f"{uuid.uuid4()}@test.com"
+
     response = client.post("/users/register", json={
-        "email": "test@test.com",
+        "email": email,
         "password": "123456"
     })
+
     assert response.status_code == 200
 
+
 def test_login():
-    response = client.post("/users/login", json={
-        "email": "test@test.com",
+    email = f"{uuid.uuid4()}@test.com"
+
+    # first register
+    client.post("/users/register", json={
+        "email": email,
         "password": "123456"
     })
+
+    # then login
+    response = client.post("/users/login", json={
+        "email": email,
+        "password": "123456"
+    })
+
     assert response.status_code == 200
